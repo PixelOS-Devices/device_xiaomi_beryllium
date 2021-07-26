25027781e125cbba2491c1fc39abdc4811d82310
Tim Zimmermann <tim@linux4.de>
2021-07-26 21:13:30 +0530

    beryllium: Flash dummy super image if needed
    * Needed because stock does not use dynamic partitions so
      there's no existing metadata that could be used
    
    Change-Id: Icbc828389465f3d4681ca8e4c3adec782540752e
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>

--- device.mk
+++ device.mk
@@ -268,6 +268,9 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/perf/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/partitions/flash_super_dummy.sh:install/bin/flash_super_dummy.sh
+
 # Perfd (dummy)
 PRODUCT_PACKAGES += \
     libqti-perfd-client
