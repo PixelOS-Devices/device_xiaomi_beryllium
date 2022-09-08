743fd5c5e14b3fa3ea91ec658c942564eb8c3e42
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-09-08 23:40:58 +0200

    beryllium: Retrofit dynamic partitions
    * Merge system, vendor and cust, giving us a whopping 5.1GB of space, pretty nice
    * Mark standard block devices as super block devices (from https://source.android.com/devices/tech/ota/dynamic_partitions/implement?hl=en#selinux-legacy)
    
    Change-Id: Ib8c8ea5ea5d93071097e79ae60fc98a72096aa1a

--- device.mk
+++ device.mk
@@ -262,6 +262,9 @@ PRODUCT_PACKAGES += \
     TetheringConfigOverlay \
     WifiOverlay
 
+# Partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
 # Perf
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/perf/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
